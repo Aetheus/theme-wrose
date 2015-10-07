5eb119b5e753b5abc5825ff448c1be9a407cee0d
Adrian <adrianhyk@gmail.com>
2015-10-08 03:56:19 +0800

    RC candidate 1

--- js/site.js
+++ js/site.js
@@ -1,30 +1,37 @@
 
 $("document").ready(function (){
 
-    $(".navbar-nav > li > a").on("click", function(ev){
-        $(".navbar-toggle").click();
-        console.log("hi");
+
+    /*on mobile screens, collapses navbar after a link is clicked*/
+    $(".navbar-nav * a").not("a.dropdown-toggle").on("click", function(ev){
+        $(".navbar-toggle").filter(":visible").click(); //only clicks the navbar toggle if its visible - and if it's on a large screen, it won't be
     });
 
-    /*
-    var officeLocation = new google.maps.LatLng(3.114588, 101.612075);
+    /*smooth scroll for navbar, plus fixes navbar dropdown*/
+    $(".navbar-nav * a").on('click', function(event){
+        event.preventDefault();
 
-    function initialize() {
-      var mapProp = {
-        center:officeLocation,
-        zoom:20,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-      };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+        if( $(this).data("target") != undefined ){
+            var hash = "#insurance-policies";
+        }else{
+            var hash = this.hash;
+        }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    });
 
-    var marker=new google.maps.Marker({
-        position:officeLocation,
-    });*/
+    /*when the navbar option for an insurance policy is clicked, go there and simulate clicking on the pill for it*/
+    $("#navbar-insurance-dropdown-list > li > a").on("click", function (ev){
+        var $thisLink = $(this);
+        $('a.insurance-pill[href = '+$thisLink.data("target")+']').click();
+    });
 
-    var myCenter=new google.maps.LatLng(3.114362, 101.612465);
+    /*g-maps API stuff*/
+    var myCenter=new google.maps.LatLng(3.114367, 101.612444);
     function initialize()
     {
         var mapProp = {
@@ -44,18 +51,32 @@ $("document").ready(function (){
     google.maps.event.addDomListener(window, 'load', initialize);
 
 
-    $(".insurances").on("click", function(ev){
-
-        var $thisInsurance =  $(this);
-
-        $("#insurance-list > li").removeClass("active");
-        $thisInsurance.parent().addClass("active");
-
-        $("#insurance-content > h2").text($thisInsurance.text());
-
-        ev.preventDefault();
-        ev.stopPropagation();
-
+    /*Disable auto-carousel if user clicks on either the left/right controls or the indicators*/
+    $(".carousel-control, .carousel-indicators > li").click(function (){
+        $(".carousel").carousel("pause");
     });
+
+    /*quick hack to fix the non-functioning carousel indicators*/
+    $("#news-carousel").on("slid.bs.carousel", function (){
+        var itemPos = 0;
+        $("#news-carousel > .carousel-inner > .item").each(function(){
+            console.log("hi");
+            var $thisItem = $(this);
+            if ($thisItem.hasClass("active")){
+                $("li[data-target='#news-carousel']").each(function(){
+                    if( $(this).data("slide-to") == itemPos ){
+                        $(this).addClass("active");
+                    }else{
+                        $(this).removeClass("active");
+                    }
+                });
+            }
+            itemPos++;
+        });
+    });
+    $(".carousel-indicators > li").on("click", function (ev){
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+
 
 });
